78a85ea617ae5fa565c14deac7d61f0a12f0d29d
Jaeyoung <103552850+Wodud5683@users.noreply.github.com>
2024-05-20 15:51:34 +0900

    feat: 첫 번째 문제

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
+import React, { useRef } from "react";
+
 export default function App() {
+  const inputRef = useRef(null);
+  const resultRef = useRef(null);
+  let result = 0;
+
+  const handleAddition = () => {
+    const inputValue = Number(inputRef.current.value);
+    result += inputValue;
+    resultRef.current.textContent = result;
+  };
+
+  const handleSubtraction = () => {
+    const inputValue = Number(inputRef.current.value);
+    result -= inputValue;
+    resultRef.current.textContent = result;
+  };
+
+  const handleReset = () => {
+    result = 0;
+    inputRef.current.value = "";
+    resultRef.current.textContent = result;
+  };
+
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-        <button>초기화</button>
+        <input ref={inputRef} type="number" /> 만큼을{" "}
+        <button onClick={handleAddition}>더할게요</button>{" "}
+        <button onClick={handleSubtraction}>뺄게요</button>
+        <button onClick={handleReset}>초기화</button>
       </div>
       <hr />
       <div>
-        <h3>결과 테스트</h3>
-        <p>0</p>
+        <h3>결과</h3>
+        <p ref={resultRef}>0</p>
       </div>
     </div>
   );
